eb054d388ccd978cc17e60859993f10078bed822
Vasilis Koutsopoulos <vaskou@hotmail.com>
2024-08-27 09:49:55 +0300

    -WIP

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-polyfill'), 'version' => 'cc84b869674649cf9cd9');
+<?php return array('dependencies' => array('react', 'react-dom', 'regenerator-runtime'), 'version' => 'cc84b869674649cf9cd9');
